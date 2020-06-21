574c8c6fa1eca4092575626d363dfd480d0ad05e
Kaivan Doshi <kaivandoshi1997@gmail.com>
2020-06-21 21:59:01 +0530

    Create stage2.py

---
+++ Zookeeper/stage2.py
@@ -0,0 +1,21 @@
+print('Switching on camera from habitat with camels...')
+print(r"""
+Switching on camera from habitat with camels...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;     
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Yey, our little camel is sunbathing!""")
